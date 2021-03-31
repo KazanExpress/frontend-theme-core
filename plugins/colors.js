613450c4c1efd4688d3176d37d2424313192e4b9
Karen <dev.kaskar2008@gmail.com>
2021-03-31 18:50:31 +0300

    Update color modificators
    Add new set of rules with backward-compatibility
    https://app.asana.com/0/1199572358508724/1200093460684686/f

--- plugins/colors.js
+++ plugins/colors.js
@@ -1,25 +1,78 @@
-module.exports = function ({ baseColors, fnName }) {
+module.exports = function ({ baseColors }) {
   const bases = baseColors[ 0 ];
   return function (style) {
     for (let key in bases) {
-      style.define(key, function (type) {
+      style.define(key, function (...types) {
         let value = bases[ key ];
         let parsed = value.match(/hsla\((.*)\)/)[ 1 ].split(',');
-        let hsla = new style.nodes.HSLA(
-          new style.nodes.Unit(parseInt(parsed[ 0 ]), 'hue').val,
-          new style.nodes.Unit(parseInt(parsed[ 1 ]), 'saturation').val,
-          new style.nodes.Unit(parseInt(parsed[ 2 ]), 'lightness').val,
-          new style.nodes.Unit(parseInt(parsed[ 3 ]), 'alpha').val
-        )
 
-        if (type && type.name) {
-          let args = new style.nodes.Arguments;
-          args.push(type)
-          args.push(hsla)
-          return new style.nodes.Call(fnName, args)
-        } else {
-          return hsla
+        const colorParts = [
+          'hue',
+          'saturation',
+          'lightness',
+          'alpha'
+        ];
+
+        const hslaParts = colorParts.reduce((acc, cur, ind) => {
+          acc[cur] = parseInt(parsed[ind]);
+          return acc;
+        }, {});
+
+        const modMap = {
+          LT: {
+            direction: 1,
+            target: 'lightness',
+          },
+          L: {
+            direction: 1,
+            target: 'lightness',
+          },
+          DK: {
+            direction: -1,
+            target: 'lightness',
+          },
+          D: {
+            direction: -1,
+            target: 'lightness',
+          },
+          ST: {
+            direction: 1,
+            target: 'saturation',
+          },
+          DS: {
+            direction: -1,
+            target: 'saturation',
+          },
+          A: {
+            direction: 0.01,
+            target: 'alpha',
+            base: 0,
+          },
+        };
+
+        for (arg of types) {
+          if (arg && !arg.isNull) {
+            const action = arg.name.match(/([A-Z]{1,2})/)[0];
+            let amount = arg.name.match(/([0-9]){1,}/)[0];
+            let newPart = modMap[action].base !== undefined ? modMap[action].base : hslaParts[modMap[action].target]
+
+            if (modMap[action]) {
+              amount *= modMap[action].direction;
+              newPart += amount;
+
+              hslaParts[modMap[action].target] = newPart;
+            }
+          }
         }
+
+        let hsla = new style.nodes.HSLA(
+          ...(Object
+              .keys(hslaParts)
+              .map(el => new style.nodes.Unit(hslaParts[el], el).val)
+          ),
+        );
+
+        return hsla;
       });
     }
   };
